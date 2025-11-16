48a67c7d4710af9e98af1633d003248ec216ce00
Niki Simaei <nikisimaei@Keirans-MacBook-Air.local>
2025-11-16 20:16:02 +0000

    updated readme

--- flex_living/settings.py
+++ flex_living/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z814f0dhhekai)xml^$86h=78pvt#^(&bh3*l@dk_rse#zsnp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
